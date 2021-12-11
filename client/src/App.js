680bc6ff56d7bb44ca71b01ca420a78bf2067bd7
Sergei <sergei.issaev@vooban.com>
2021-12-11 12:20:05 -0500

    “websocket”

--- client/src/App.js
+++ client/src/App.js
@@ -7,24 +7,32 @@ console.log(host)
 
 class App extends Component {
   // Initialize state
-  state = { banana: true }
+  state = { me: "placeholder" }
 
   // Fetch passwords after first mount
   componentDidMount() {
+    const io = require("socket.io-client");     
+    const socket = io.connect(host, {port: port, transports: ["websocket"]});
 
+
+    socket.on("me", (id) => {
+			this.setState({me: id})
+			console.log('setting variable me to')
+			console.log(this.state.me)
+		})
   }
 
 
 
   render() {
-    const io = require("socket.io-client");     
-    const socket = io.connect(host, {port: port, transports: ["websocket"]});
-    console.log(socket)
+
+
 
     return (
       <div >
       <h1>Hi Yuyan!</h1>
       <h2>Want to get <b>Popeyes!!?</b></h2>
+      <h1>{this.state.me}</h1>
       <button onClick={()=> alert('Yay! I just received the notification, and will be outside in 5 minutes!')} type="button">Yes! Let's Go!</button> 
       <button onClick={()=> alert('I know.')} type="button">I'm a butt :(</button> 
       </div>
